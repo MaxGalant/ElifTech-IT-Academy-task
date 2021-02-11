d20d3b9e123f2923510824d6ca92f26d24fe5d1b
MaxGalant <galantmaks01@gmail.com>
2021-02-12 00:14:10 +0200

    1

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,8 @@
 const mySql=require("mysql")
 const db =mySql.createConnection({
-
+    host: "localhost",
+    user: 'root',
+    password: "password",
+    database: "ami_sa_db"
 })
 module.exports =db
